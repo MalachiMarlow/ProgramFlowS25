aea735aec5810c58330991aa53fbdcd85a8fa8f8
Tusk <Tuskman045@gmail.com>
2025-01-27 08:34:43 -0700

    Add if, & if, ifelse examples

--- ProgramFlowS25/ProgramFlow.vb
+++ ProgramFlowS25/ProgramFlow.vb
@@ -1,7 +1,41 @@
-﻿Module ProgramFlow
+﻿'Malachi Marlow
+'Spring 2025
+'RCET2265
+'ProgramFlowS25
+
+Option Explicit On
+Option Strict On
+
+Module ProgramFlow
 
     Sub Main()
+        Dim user As Integer '1 & 0
+        '
+        'assign 5 to user
+        user = 5
 
+        'test is user greater than 6
+        'display the result on the console
+        If user > 6 Then
+            Console.WriteLine(user > 6)
+        End If
+
+        If user > 3 Then
+            Console.WriteLine(user > 3)
+        End If
+
+        If user > 4 Then
+            Console.WriteLine(user > 4)
+        End If
+
+        'If anything evaluates to true, ends if statement
+        If user > 6 Then
+            Console.WriteLine(user > 6)
+        ElseIf user > 3 Then
+            Console.WriteLine(user > 3)
+        ElseIf user > 4 Then
+            Console.WriteLine(user > 4)
+        End If
     End Sub
 
 End Module
